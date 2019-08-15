506626c6030fc0999dfcfc6b637fc49fb59bcee5
cgp <123@qq.com>
2019-08-15 09:46:43 +0800

    二次

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -15,6 +15,10 @@ namespace WindowsFormsApplication1
         static void Main()
         {
 
+
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
